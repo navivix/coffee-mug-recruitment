96548b4c2d28d837de90ea3523a06ae5d92e9532
navivix <gkaczmarek2304@gmail.com>
2022-11-15 14:36:45 +0100

    Use PUT instead of PATCH for update

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,10 +3,10 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
   ParseIntPipe,
+  Put,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -31,7 +31,7 @@ export class ProductsController {
     return this.productsService.findOne(id);
   }
 
-  @Patch(':id')
+  @Put(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
