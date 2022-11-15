c0ca2cefe6408a7e70cac85aaf6a302f443af71a
navivix <gkaczmarek2304@gmail.com>
2022-11-15 14:20:32 +0100

    Add create-product dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,11 @@
-export class CreateProductDto {}
+import { IsInt, IsNotEmpty, MaxLength } from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @MaxLength(100)
+  name: string;
+
+  // Price in pennies, for precision
+  @IsInt()
+  price: number;
+}
