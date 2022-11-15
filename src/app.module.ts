835776a6b36e7dde0b1ce4029670e368435c9370
navivix <gkaczmarek2304@gmail.com>
2022-11-15 15:35:10 +0100

    Use ConfigModule to support .env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT) || 3306,
-      username: process.env.DB_USERNAME || 'root',
-      password: process.env.DB_PASSWORD || '',
-      database: process.env.DB_NAME || 'test',
-      autoLoadEntities: true,
-      synchronize: process.env.NODE_ENV !== 'production',
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST') || 'localhost',
+        port: parseInt(configService.get('DB_PORT')) || 3306,
+        username: configService.get('DB_USERNAME') || 'root',
+        password: configService.get('DB_PASSWORD') || '',
+        database: configService.get('DB_NAME') || 'test',
+        autoLoadEntities: true,
+        synchronize: configService.get('NODE_ENV') !== 'production',
+      }),
+      inject: [ConfigService],
     }),
     ProductsModule,
   ],
