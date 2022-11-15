d10c1e31b82529ad23d55b40fd66af8d04d8d339
navivix <gkaczmarek2304@gmail.com>
2022-11-15 14:46:04 +0100

    Use ParseIntPipe in PUT and DELETE

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -32,12 +32,15 @@ export class ProductsController {
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.remove(id);
   }
 }
