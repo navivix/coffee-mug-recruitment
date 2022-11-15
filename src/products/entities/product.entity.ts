c97f51143316577e8265930259eb8857afb7d958
navivix <gkaczmarek2304@gmail.com>
2022-11-15 13:51:26 +0100

    Add product entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,21 @@
-export class Product {}
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  price: number;
+
+  @UpdateDateColumn()
+  updateDate: Date;
+}
